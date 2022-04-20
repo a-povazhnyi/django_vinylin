11abd8f3c913fbc7662613abc2fb8f16c208a9c1
a-povazhnyi <a.povazhnyi@wis.software>
2022-04-21 00:23:52 +0300

    fix: Country model

--- vinylin/vinyl/models.py
+++ vinylin/vinyl/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 
-from .data import countries
-
 
 class Country(models.Model):
-    name = models.CharField(max_length=200, choices=countries.COUNTRY_CHOICES)
+    name = models.CharField(max_length=200, unique=True)
+
+    def __str__(self):
+        return self.name
 
     class Meta:
         verbose_name = 'Country'
         verbose_name_plural = 'Countries'
+        ordering = ['pk']
