2195d0671bdfd8901a62473fd78e9c581fc0ceac
a-povazhnyi <a.povazhnyi@wis.software>
2022-04-20 16:44:09 +0300

    feat: add Country model

--- vinylin/vinyl/models.py
+++ vinylin/vinyl/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+from .data import countries
+
+
+class Country(models.Model):
+    name = models.CharField(max_length=200, choices=countries.COUNTRY_CHOICES)
+
+    class Meta:
+        verbose_name = 'Country'
+        verbose_name_plural = 'Countries'
