ad83cbd2413dec0bd288b2125c418f8d94871bc0
a-povazhnyi <a.povazhnyi@wis.software>
2022-04-21 23:15:20 +0300

    feat: add SignOut view, url changes

--- vinylin/users/views.py
+++ vinylin/users/views.py
@@ -1,12 +1,25 @@
 from django.shortcuts import render, redirect
-from django.views.generic import CreateView
-from django.contrib.auth import login
+from django.views.generic import TemplateView, CreateView
+from django.contrib.auth import login, logout
 
 from .forms import UserForm
 
 
 def sign_in(request):
     return render(request, 'users/signin.html')
+
+
+class SignOut(TemplateView):
+    template_name = 'vinyl/index.html'
+
+    def get(self, request, *args, **kwargs):
+        logout(request)
+        return redirect('index')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['alert'] = 'You successfully signed out!'
+        return context
 
 
 class Register(CreateView):
