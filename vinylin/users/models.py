11de7cb4add729496959b3b61918f16df2d09551
a-povazhnyi <a.povazhnyi@wis.software>
2022-04-21 03:58:50 +0300

    feat: add profile model

--- vinylin/users/models.py
+++ vinylin/users/models.py
@@ -1,8 +1,19 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+from vinyl.models import Country
 
-# class Profile(models.Model):
-#     user_id = models.OneToOneField(User, on_delete=models.CASCADE)
-#     phone = models.CharField()
-#     age = models.IntegerField()
+
+class Profile(models.Model):
+    user_id = models.OneToOneField(User, on_delete=models.CASCADE)
+    is_email_verified = models.BooleanField(default=False)
+    phone = models.CharField(max_length=20, blank=True)
+    age = models.IntegerField(blank=True)
+    country = models.ForeignKey(
+        Country,
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name='country')
+
+    def __str__(self):
+        return f'{self.user_id} ({self.pk})'
