10b7557e577deaae1ea9d92af402cc4ca7ecfe79
a-povazhnyi <a.povazhnyi@wis.software>
2022-04-21 22:37:49 +0300

    feat: add Register view

--- vinylin/users/views.py
+++ vinylin/users/views.py
@@ -1,9 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.generic import CreateView
+from django.contrib.auth import login
+
+from .forms import UserForm
 
 
 def sign_in(request):
     return render(request, 'users/signin.html')
 
 
-def register(request):
-    return render(request, 'users/register.html')
+class Register(CreateView):
+    def get(self, request, *args, **kwargs):
+        user_form = UserForm()
+        context = {'user_form': user_form}
+        return render(request, 'users/register.html', context)
+
+    def post(self, request, *args, **kwargs):
+        user_form = UserForm(data=request.POST)
+
+        if user_form.is_valid():
+            new_user = user_form.save()
+            login(request, new_user)
+            return redirect('https://www.google.com/')
