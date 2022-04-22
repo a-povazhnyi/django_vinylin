937e32cb8c48974d4d206886cf60aac2c7980d4f
a-povazhnyi <a.povazhnyi@wis.software>
2022-04-22 14:41:43 +0300

    fix: add balance field to Profile model

--- vinylin/users/models.py
+++ vinylin/users/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 from vinyl.models import Country
 
@@ -14,6 +16,7 @@ class Profile(models.Model):
         on_delete=models.SET_NULL,
         null=True,
         related_name='country')
+    balance = models.DecimalField(default=0, max_digits=6, decimal_places=2)
 
     def __str__(self):
         return f'{self.user_id} ({self.pk})'
