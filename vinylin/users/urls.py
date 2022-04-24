6ed3b1cf7057ed7257ab5a4235c157d572d0d012
a-povazhnyi <a.povazhnyi@wis.software>
2022-04-25 01:46:16 +0300

    feat: add sign-exceptions url

--- vinylin/users/urls.py
+++ vinylin/users/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
 
-from .views import (SignIn, SignOut, Register)
+from .views import (SignIn, SignOut, Register, SignExceptions)
 
 urlpatterns = [
     path('sign-in/', SignIn.as_view(), name='sign-in'),
     path('sign-out/', SignOut.as_view(), name='sign-out'),
     path('register/', Register.as_view(), name='register'),
+    path('sign-exceptions/', SignExceptions.as_view(), name='sign_exceptions'),
 ]
