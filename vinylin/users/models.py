955a98054b58d5511968c254dbd7cf4ff0a24e5e
a-povazhnyi <a.povazhnyi@wis.software>
2022-04-25 03:01:17 +0300

    feat: add custom User model

--- vinylin/users/models.py
+++ vinylin/users/models.py
@@ -1,15 +1,19 @@
 from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
-from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 from vinyl.models import Country
 
 
-class Profile(models.Model):
-    user_id = models.OneToOneField(User, on_delete=models.CASCADE)
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
     is_email_verified = models.BooleanField(default=False)
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     phone = models.CharField(max_length=20, blank=True, null=True)
     age = models.SmallIntegerField(
         validators=[MinValueValidator(0), MaxValueValidator(170)],
@@ -25,10 +29,10 @@ class Profile(models.Model):
     balance = models.DecimalField(default=0, max_digits=6, decimal_places=2)
 
     def __str__(self):
-        return f'{self.user_id} ({self.pk})'
+        return f'{self.user} ({self.pk})'
 
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
-        Profile.objects.create(user_id=instance)
+        Profile.objects.create(user=instance)
