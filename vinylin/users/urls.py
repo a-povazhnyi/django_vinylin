3f675befd4203977b40e4c70ac225ee42a1d420d
a-povazhnyi <a.povazhnyi@wis.software>
2022-04-26 18:19:47 +0300

    feat: add email urls

--- vinylin/users/urls.py
+++ vinylin/users/urls.py
@@ -1,10 +1,23 @@
 from django.urls import path, include
 
-from .views import (SignIn, SignOut, Register, SignExceptions)
+from .views import (
+    SignIn, SignOut, Register, SignExceptions,
+    EmailVerification, EmailChange, EmailConfirm
+)
 
 urlpatterns = [
     path('sign-in/', SignIn.as_view(), name='sign-in'),
     path('sign-out/', SignOut.as_view(), name='sign-out'),
     path('register/', Register.as_view(), name='register'),
     path('sign-exceptions/', SignExceptions.as_view(), name='sign_exceptions'),
+
+    path('email-verification/',
+         EmailVerification.as_view(),
+         name='email_verification'),
+    path('email-confirm/<str:verification_code>/',
+         EmailConfirm.as_view(),
+         name='email_confirm'),
+    path('email-change/',
+         EmailChange.as_view(),
+         name='email_change'),
 ]
