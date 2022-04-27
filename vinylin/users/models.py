6c5aaebb620809266f010da5157ce6525caabeda
a-povazhnyi <a.povazhnyi@wis.software>
2022-04-27 13:28:49 +0300

    fix: get_absolute_url for User

--- vinylin/users/models.py
+++ vinylin/users/models.py
@@ -3,6 +3,7 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 from django.contrib.auth.models import AbstractUser
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django.urls import reverse
 
 from vinyl.models import Country
 
@@ -10,6 +11,12 @@ from vinyl.models import Country
 class User(AbstractUser):
     email = models.EmailField(unique=True)
     is_email_verified = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'{self.username} ({self.pk})'
+
+    def get_absolute_url(self):
+        return reverse('profile', kwargs={'pk': self.pk})
 
 
 class Profile(models.Model):
@@ -25,7 +32,8 @@ class Profile(models.Model):
         on_delete=models.SET_NULL,
         blank=True,
         null=True,
-        related_name='country')
+        related_name='country'
+    )
     balance = models.DecimalField(default=0, max_digits=6, decimal_places=2)
 
     def __str__(self):
