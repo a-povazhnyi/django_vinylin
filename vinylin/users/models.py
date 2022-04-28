f5d9b2ddcd7a96e869dd56a15ed63c274bc2eee3
a-povazhnyi <a.povazhnyi@wis.software>
2022-04-28 22:11:57 +0300

    fix: add phonenumber field to Profile

--- vinylin/users/models.py
+++ vinylin/users/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import AbstractUser
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.urls import reverse
+from phonenumber_field.modelfields import PhoneNumberField
 
 from vinyl.models import Country
 
@@ -21,7 +22,8 @@ class User(AbstractUser):
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    phone = models.CharField(max_length=20, blank=True, null=True)
+    # phone = models.CharField(max_length=20, blank=True, null=True)
+    phone = PhoneNumberField(blank=True, null=True)
     age = models.SmallIntegerField(
         validators=[MinValueValidator(0), MaxValueValidator(170)],
         blank=True,
