575e558751274f2bf211609113c1434dd997f312
a-povazhnyi <a.povazhnyi@wis.software>
2022-04-28 19:31:06 +0300

    fix: rename url name for sign-(in/out)

--- vinylin/users/urls.py
+++ vinylin/users/urls.py
@@ -9,8 +9,8 @@ from .views import (
 )
 
 urlpatterns = [
-    path('sign-in/', SignInView.as_view(), name='sign-in'),
-    path('sign-out/', SignOutView.as_view(), name='sign-out'),
+    path('sign-in/', SignInView.as_view(), name='sign_in'),
+    path('sign-out/', SignOutView.as_view(), name='sign_out'),
     path('register/', RegisterView.as_view(), name='register'),
     path('<int:pk>/', ProfileView.as_view(), name='profile'),
     path('sign-exceptions/',
