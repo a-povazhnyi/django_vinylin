02450ec5ae073272c4c01a576854b703a080ed2b
a-povazhnyi <a.povazhnyi@wis.software>
2022-05-05 16:33:21 +0300

    fix: few model fixes

--- vinylin/store/models.py
+++ vinylin/store/models.py
@@ -1,4 +1,8 @@
 from django.db import models
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+from django.core.exceptions import ObjectDoesNotExist
+from django.core.validators import MinValueValidator
 
 
 class Category(models.Model):
@@ -19,13 +23,13 @@ class Tag(models.Model):
         return self.title
 
 
-class Sale(models.Model):
-    product = models.OneToOneField(
+class Discount(models.Model):
+    product = models.ForeignKey(
         to='Product',
-        null=True,
-        on_delete=models.SET_NULL,
+        on_delete=models.CASCADE,
+        related_name='discount',
     )
-    amount = models.IntegerField()
+    amount = models.SmallIntegerField(default=0)
     created_at = models.DateTimeField(auto_now_add=True)
 
 
@@ -33,6 +37,7 @@ class Image(models.Model):
     product = models.ForeignKey(
         to='Product',
         on_delete=models.CASCADE,
+        related_name='images',
     )
     image = models.ImageField()
 
@@ -43,7 +48,12 @@ class Image(models.Model):
 class AbstractProduct(models.Model):
     title = models.CharField(max_length=250)
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    part_number = models.CharField(max_length=100, null=True)
+    part_number = models.CharField(
+        max_length=11,
+        unique=True,
+        blank=True,
+        null=True,
+    )
     overview = models.TextField(blank=True, null=True)
 
     class Meta:
@@ -58,18 +68,44 @@ class Product(AbstractProduct):
     tags = models.ManyToManyField(Tag)
     created_at = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return self.title
+
+    @property
+    def price_with_discount(self):
+        try:
+            discount_amount = self.discount.get(product=self).amount
+            discount_amount *= 0.01
+        except ObjectDoesNotExist:
+            return None
+        return self.price * (1 - discount_amount)
+
+
+@receiver(post_save, sender=Product)
+def create_storage_obj(sender, instance, created, **kwargs):
+    """Creates Storage object after product creation"""
+    if created:
+        Storage.objects.create(product=instance)
+
 
 class Storage(models.Model):
     product = models.OneToOneField(
         to=Product,
         on_delete=models.CASCADE,
+        related_name='storage',
     )
-    quantity = models.IntegerField(default=1)
+    quantity = models.IntegerField(
+        default=1,
+        validators=[MinValueValidator(0)],
+    )
+
+    def __str__(self):
+        return self.product.title
 
     @property
-    def status(self):
+    def bad_status(self):
         if self.quantity < 1:
             return 'out_of_stock'
         if self.quantity < 10:
             return 'running_low'
-        return 'in_stock'
+        return False
