f3bedb78e249f8f5185508565f76d26e63f73183
a-povazhnyi <a.povazhnyi@wis.software>
2022-05-08 22:16:15 +0300

    fix: add normal view for index

--- vinylin/vinyl/views.py
+++ vinylin/vinyl/views.py
@@ -1,5 +1,15 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
+
+from .models import Vinyl
 
 
-class IndexView(TemplateView):
+class IndexView(ListView):
     template_name = 'vinyl/index.html'
+    context_object_name = 'vinyls'
+
+    def get_queryset(self):
+        return Vinyl.objects.with_index_data().all()
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
