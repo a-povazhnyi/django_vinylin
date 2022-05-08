b8ac895fd2eb1c8f07d230033823fe6254ec3de5
a-povazhnyi <a.povazhnyi@wis.software>
2022-05-08 22:17:00 +0300

    fix: import debug_toolbar with debug settings, media urls

--- vinylin/vinylin/urls.py
+++ vinylin/vinylin/urls.py
@@ -1,11 +1,22 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
-    path('__debug__/', include('debug_toolbar.urls')),
-
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
 
     path('', include('vinyl.urls'))
 ]
+
+if settings.DEBUG:
+    import debug_toolbar
+
+    urlpatterns = [
+        path('__debug__/', include(debug_toolbar.urls))
+    ] + urlpatterns
+
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
