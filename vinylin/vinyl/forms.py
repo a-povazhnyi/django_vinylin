644d7f589946f41ff903dbda6cdc690a2f9f59fd
a-povazhnyi <a.povazhnyi@wis.software>
2022-05-09 23:42:46 +0300

    feat: register ckeditor widget for overview field

--- vinylin/vinyl/forms.py
+++ vinylin/vinyl/forms.py
@@ -1,10 +1,12 @@
 from django import forms
+from ckeditor_uploader.widgets import CKEditorUploadingWidget
 
 from .models import Vinyl
 from store.utils import generate_part_number
 
 
 class VinylAdminForm(forms.ModelForm):
+    overview = forms.CharField(widget=CKEditorUploadingWidget())
     class Meta:
         model = Vinyl
         fields = '__all__'
