89d6b429b2aeaa639db4a3bc2ba7e61013e4b6ab
a-povazhnyi <a.povazhnyi@wis.software>
2022-05-09 23:40:28 +0300

    feat: add with_all_data func

--- vinylin/vinyl/managers.py
+++ vinylin/vinyl/managers.py
@@ -6,3 +6,11 @@ class VinylManager(models.Manager):
         return self.prefetch_related('images') \
             .prefetch_related('tags') \
             .select_related('discount')
+
+    def with_all_data(self):
+        return self.prefetch_related('images') \
+            .prefetch_related('tags') \
+            .prefetch_related('genres') \
+            .select_related('country') \
+            .select_related('discount') \
+            .select_related('artist')
