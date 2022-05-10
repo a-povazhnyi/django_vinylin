f2e5ed944ef9faba6fe336163c93956e5f3cccdf
a-povazhnyi <a.povazhnyi@wis.software>
2022-05-10 12:53:13 +0300

    feat: register models

--- vinylin/orders/admin.py
+++ vinylin/orders/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Cart, Order, OrderItems
+
+
+admin.site.register(Cart)
+admin.site.register(Order)
+admin.site.register(OrderItems)
