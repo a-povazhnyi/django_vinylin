4d86e0cbe2acdb43afb1746ad36f9ac456914d11
a-povazhnyi <a.povazhnyi@wis.software>
2022-05-11 05:59:44 +0300

    feat: CartView init

--- vinylin/orders/views.py
+++ vinylin/orders/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.db.models import F
+from django.views.generic import DetailView
+from django.shortcuts import redirect
 
-# Create your views here.
+from .models import Cart, OrderItem
+
+
+class CartView(DetailView):
+    template_name = 'orders/cart.html'
+
+    def get_queryset(self):
+        return Cart.objects.filter(pk=self.kwargs['pk'])
+
+    def add_to_cart(self, cart_pk, product_pk):
+        order_item, created_order_item = OrderItem.objects.get_or_create(
+            cart_id=cart_pk,
+            order=None,
+            product_id=product_pk,
+        )
+        if order_item:
+            order_item.quantity = F('quantity') + 1
+            order_item.save()
+        return redirect('vinyl_single', pk=product_pk)
