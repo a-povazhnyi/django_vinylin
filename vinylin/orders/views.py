73506210260536922bcd13afc618c9c48b43eccd
a-povazhnyi <a.povazhnyi@wis.software>
2022-05-13 14:18:20 +0300

    feat: post method

--- vinylin/orders/views.py
+++ vinylin/orders/views.py
@@ -1,23 +1,39 @@
 from django.db.models import F
-from django.views.generic import DetailView
+from django.views.generic import ListView
 from django.shortcuts import redirect
 
-from .models import Cart, OrderItem
+from .forms import OrderItemQuantityForm
+from .models import OrderItem
 
 
-class CartView(DetailView):
+class CartView(ListView):
     template_name = 'orders/cart.html'
+    context_object_name = 'cart_items'
 
     def get_queryset(self):
-        return Cart.objects.filter(pk=self.kwargs['pk'])
+        return OrderItem.objects.filter(cart_id=self.kwargs['pk'])\
+            .order_by('product_id')
 
     def add_to_cart(self, cart_pk, product_pk):
-        order_item, created_order_item = OrderItem.objects.get_or_create(
+        order_item, created_order_item = OrderItem.objects.update_or_create(
             cart_id=cart_pk,
             order=None,
             product_id=product_pk,
+
+            defaults={'quantity': F('quantity') + 1}
         )
-        if order_item:
-            order_item.quantity = F('quantity') + 1
-            order_item.save()
-        return redirect('vinyl_single', pk=product_pk)
+        return redirect('cart', pk=cart_pk)
+
+    def post(self, request, *args, **kwargs):
+        order_item_id = int(request.POST['order_item_id'])
+        order_item_obj = OrderItem.objects.get(pk=order_item_id)
+        form = OrderItemQuantityForm(
+            data=request.POST,
+            instance=order_item_obj
+        )
+
+        if not form.is_valid():
+            return redirect('cart', pk=request.user.cart.pk)
+
+        form.save()
+        return redirect('cart', pk=request.user.cart.pk)
