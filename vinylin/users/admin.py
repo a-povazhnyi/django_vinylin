24253c717b4d345f38fa9d9c5accc2df3f7e4fae
a-povazhnyi <a.povazhnyi@wis.software>
2022-05-18 15:40:13 +0300

    feat: ProfileAdmin init

--- vinylin/users/admin.py
+++ vinylin/users/admin.py
@@ -1,7 +1,29 @@
 from django.contrib import admin
+from django.urls import path
 
+from .views import AddBalanceAdminView
 from .models import User, Profile
 
 
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user')
+    list_display_links = ('id', 'user')
+    save_on_top = True
+
+    def get_urls(self):
+        urls = super().get_urls()
+        custom_urls = [
+            path('balance/',
+                 self.add_balance_view,
+                 name='add_balance')
+        ]
+        urls = custom_urls + urls
+        return urls
+
+    def add_balance_view(self, request):
+        context = dict(self.admin_site.each_context(request))
+        return AddBalanceAdminView.as_view()(request, admin_context=context)
+
+
 admin.site.register(User)
-admin.site.register(Profile)
