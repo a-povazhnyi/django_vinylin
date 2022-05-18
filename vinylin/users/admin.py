9bbf39ff21b532fbf182785095fff8e83e59867c
a-povazhnyi <a.povazhnyi@wis.software>
2022-05-18 20:52:00 +0300

    feat: full_name list field

--- vinylin/users/admin.py
+++ vinylin/users/admin.py
@@ -5,6 +5,16 @@ from .views import AddBalanceAdminView
 from .models import User, Profile
 
 
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'email', 'full_name', 'is_email_verified')
+    list_display_links = ('id', 'email', 'full_name')
+
+    @staticmethod
+    def full_name(obj):
+        return f'{obj.first_name} {obj.last_name}'
+
+
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
     change_list_template = 'admin/profile_change_list.html'
