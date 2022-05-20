7471669d8b92991a9b81cf946bfd6f2f008012ce
a-povazhnyi <a.povazhnyi@wis.software>
2022-05-20 11:35:18 +0300

    fix: del extra method

--- vinylin/vinyl/views.py
+++ vinylin/vinyl/views.py
@@ -10,10 +10,6 @@ class IndexView(ListView):
     def get_queryset(self):
         return Vinyl.objects.with_index_data().all().order_by('id')
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        return context
-
 
 class VinylDetailView(DetailView):
     template_name = 'vinyl/single.html'
