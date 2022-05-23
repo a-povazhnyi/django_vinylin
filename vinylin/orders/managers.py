fcc632454328e25cb35dd5a8a48801e11f85b4c4
a-povazhnyi <a.povazhnyi@wis.software>
2022-05-23 16:36:55 +0300

    fix: OrderItem final_price annotation

--- vinylin/orders/managers.py
+++ vinylin/orders/managers.py
@@ -1,13 +1,27 @@
-from django.db import models
+from django.db.models import Manager, DecimalField, F, Q, Case, When
 
 
-class OrderItemManager(models.Manager):
+class OrderItemManager(Manager):
     def get_queryset(self):
-        return (
+        queryset = (
             super().get_queryset()
-                   .select_related('cart')
-                   .select_related('order')
-                   .prefetch_related('product__images')
-                   .prefetch_related('product__tags')
-                   .select_related('product__discount')
+            .select_related('product', 'product__discount')
+            .prefetch_related('product__images')
+            .prefetch_related('product__tags')
+            .annotate(final_price=Case(
+                When(
+                    product__discount__amount=None,
+                    then=F('product__price') * F('quantity')
+                ),
+                When(
+                    product__discount__amount=Q(product__discount__amount__isnull=False),
+                    then=(F('product__price') * (1 - (F('product__discount__amount') * self.get_decimal(0.01)))) * F('quantity'))
+                ))
         )
+        return queryset
+
+    @staticmethod
+    def get_decimal(value):
+        return DecimalField(
+            max_digits=6, decimal_places=2
+        ).clean(str(value), None)
