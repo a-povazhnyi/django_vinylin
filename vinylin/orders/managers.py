db8b1b6ec799f64bfd8663706a08b10c3339dc67
a-povazhnyi <a.povazhnyi@wis.software>
2022-05-24 10:15:36 +0300

    fix: OrderItem manager refactoring

--- vinylin/orders/managers.py
+++ vinylin/orders/managers.py
@@ -3,22 +3,37 @@ from django.db.models import Manager, DecimalField, F, Q, Case, When
 
 class OrderItemManager(Manager):
     def get_queryset(self):
-        queryset = (
-            super().get_queryset()
-            .select_related('product', 'product__discount')
-            .prefetch_related('product__images')
-            .prefetch_related('product__tags')
-            .annotate(final_price=Case(
+        queryset = self.join_relevant(super().get_queryset())
+        queryset = self.annotate_final_price(queryset)
+        return queryset
+
+    def annotate_final_price(self, queryset):
+        price = F('product__price')
+        quantity = F('quantity')
+        discount_amount = F('product__discount__amount')
+        discount_amount_not_null = Q(product__discount__amount__isnull=False)
+        # Used to convert the discount percentage to a decimal fraction
+        dec = self.get_decimal(0.01)
+
+        return queryset.annotate(
+            final_price=Case(
                 When(
                     product__discount__amount=None,
-                    then=F('product__price') * F('quantity')
+                    then=price * quantity
                 ),
                 When(
-                    product__discount__amount=Q(product__discount__amount__isnull=False),
-                    then=(F('product__price') * (1 - (F('product__discount__amount') * self.get_decimal(0.01)))) * F('quantity'))
-                ))
+                    product__discount__amount=discount_amount_not_null,
+                    then=(price * (1 - (discount_amount * dec))) * quantity)
+            )
         )
-        return queryset
+
+    @staticmethod
+    def join_relevant(queryset):
+        return (
+            queryset.select_related('product', 'product__discount')
+                    .prefetch_related('product__images')
+                    .prefetch_related('product__tags')
+        )
 
     @staticmethod
     def get_decimal(value):
